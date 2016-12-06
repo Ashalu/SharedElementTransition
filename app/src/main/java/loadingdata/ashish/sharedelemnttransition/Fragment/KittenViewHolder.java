e4a173783eaf2c3a25ff505fe8c8931051152f60
Ashish Kumar <akbrother02@gmail.com>
2016-12-06 14:45:36 +0530

    Changing the imageView

---
+++ app/src/main/java/loadingdata/ashish/sharedelemnttransition/Fragment/KittenViewHolder.java
@@ -0,0 +1,21 @@
+package loadingdata.ashish.sharedelemnttransition.Fragment;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import loadingdata.ashish.sharedelemnttransition.R;
+
+/**
+ * ViewHolder for kitten cells in our grid
+ *
+ * @author bherbst
+ */
+public class KittenViewHolder extends RecyclerView.ViewHolder {
+    ImageView image;
+
+    public KittenViewHolder(View itemView) {
+        super(itemView);
+        image = (ImageView) itemView.findViewById(R.id.image);
+    }
+}
